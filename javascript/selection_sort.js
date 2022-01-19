4094640f15fc01d719fc9f4dfc6141007ebca471
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-01-19 14:08:59 -0800

    completed assignment

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,5 +1,15 @@
 function selectionSort(arr) {
-  // type your code here
+  for (let i = 0; i < arr.length; i++){
+    for (let j = i+1; j < arr.length; j++){
+      if (arr[i] > arr[j]){
+        let temp = arr[j]
+        arr[j] = arr[i]
+        arr[i] = temp
+      }
+    }
+  }
+  return arr
+
 }
 
 if (require.main === module) {
@@ -10,11 +20,30 @@ if (require.main === module) {
   console.log("");
 
   // BENCHMARK HERE, and print the average runtime
+  
+  
+  const startTime = new Date()
+
+  const shortInput = [];
+  for (let i = 0; i < 3; ++i) {
+    shortInput.push(Math.random());
+  }
+
   const longInput = [];
 
   for (let i = 0; i < 100; ++i) {
     longInput.push(Math.random());
   }
+
+  
+  for (let i = 0; i < 1000; ++i) {
+    selectionSort(shortInput)
+    selectionSort(longInput)
+  }
+  const currentTime = new Date()
+  const averageRuntime = (currentTime - startTime) / 2000
+  console.log(averageRuntime)
+
 }
 
 module.exports = selectionSort;
